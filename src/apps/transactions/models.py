0d753d9ad2316f2d1ecd1f7791258ef61c07c56c
M Afif Nur Azizi <afifnurazizi@gmail.com>
2021-04-09 16:01:41 +0800

    refactore: member and card number in class Transactions

--- src/apps/transactions/models.py
+++ src/apps/transactions/models.py
@@ -13,21 +13,22 @@ class PaymentMethods(models.Model):
 
 
 class Transactions(models.Model):
-    member = models.ForeignKey(Members, on_delete=models.CASCADE, related_name='members', null=True)
+    member = models.ForeignKey(Members, on_delete=models.CASCADE, related_name='members', null=True, blank=True)
     sales = models.ForeignKey(Sales, on_delete=models.CASCADE, related_name='sales')
     payment_method = models.ForeignKey(PaymentMethods, on_delete=models.CASCADE, related_name='payment_method')
-    card_number = models.IntegerField()
+    card_number = models.IntegerField(null=True, blank=True)
     paid_of = models.BooleanField(null=True)
     create_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True, null=True)
 
     def __str__(self):
-        return self.sales
+        return str(self.sales)
 
 
 class DetailTransaction(models.Model):
     transaction = models.ForeignKey(Transactions, on_delete=models.CASCADE, related_name='trans')
     detail_item = models.ForeignKey(Items, on_delete=models.CASCADE, related_name='dti')
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.transaction
+        return self.quantity
