f34f83be7bb152f21aa5c099475ebfecb1b42ddc
M Afif Nur Azizi <afifnurazizi@gmail.com>
2021-04-09 15:30:42 +0800

    create class PaymentMethodsAdmin, class TransactionsAdmin, class DetailTransactionAdmin in apps.transaction.admin.py

--- src/apps/transactions/admin.py
+++ src/apps/transactions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import PaymentMethods, Transactions, DetailTransaction
 
-# Register your models here.
+
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_at", "update_at")
+
+
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ("member", "sales", "payment_method", "card_number")
+
+
+class DetailTransactionAdmin(admin.ModelAdmin):
+    list_display = ("transaction", "detail_item")
+
+
+admin.site.register(PaymentMethods, PaymentMethodAdmin)
+admin.site.register(Transactions, TransactionsAdmin)
+admin.site.register(DetailTransaction, DetailTransactionAdmin)
