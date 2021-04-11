dfff077d07fff5e286297bf5194930785814aa45
M Afif Nur Azizi <afifnurazizi@gmail.com>
2021-04-11 19:46:00 +0800

    feat : create template base with bootstrap

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from apps.accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', views.Index.as_view()),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
