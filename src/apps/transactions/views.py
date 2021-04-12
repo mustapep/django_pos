710118a9aad8d23bfe343d15531cc89b29edaeec
M Afif Nur Azizi <afifnurazizi@gmail.com>
2021-04-12 23:46:18 +0800

    feat: displays sales name, create of transaction, total_item, and bill

--- src/apps/transactions/views.py
+++ src/apps/transactions/views.py
@@ -9,7 +9,7 @@ class ListTransactionView(View):
 
     def get(self, request):
 
-        t_all = Transactions.objects.all()
+        t_all = Transactions.objects.filter(paid_of=False)
 
         return render(request, self.template_name, {
             "t_all": t_all
@@ -22,11 +22,16 @@ class DetailTransactionView(View):
 
     def get(self, request, id):
         trn = Transactions.objects.get(id=id)
-        dt = DetailTransaction.objects.filter(transaction=trn).select_related('transaction', 'detail_item')
+        dt = DetailTransaction.objects.filter(transaction=trn)
         total = []
+        total_item = []
         for d in dt:
             total.append(d.detail_item.price*d.quantity)
+            total_item.append(d.quantity)
         return render(request, self.template_name, {
             'dt': dt,
-            'total': total
+            'total': total,
+            'obj': trn,
+            't_i': sum(total_item),
+            't_p': sum(total)
         })
