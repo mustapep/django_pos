4552c5706d81526d69ed9f4b4cf58128081f37b3
M Afif Nur Azizi <afifnurazizi@gmail.com>
2021-06-26 00:53:17 +0800

    docs: change Login and RegisterView in views.py #23

--- src/apps/items/views.py
+++ src/apps/items/views.py
@@ -4,11 +4,11 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .forms import AddItemForm
 from django.http import HttpResponse
+from .item_mixin import IsAutenticate
 
 
-class ListItemView(LoginRequiredMixin, PermissionRequiredMixin, View):
+class ListItemView(LoginRequiredMixin, IsAutenticate, PermissionRequiredMixin, View):
     permission_required = [('items.view_items')]
-
     template_name = 'sales/list_item.html'
     login_url = '/login'
 
@@ -20,7 +20,7 @@ class ListItemView(LoginRequiredMixin, PermissionRequiredMixin, View):
         })
 
 
-class AddItemView(PermissionRequiredMixin, LoginRequiredMixin, View):
+class AddItemView(LoginRequiredMixin, IsAutenticate, PermissionRequiredMixin, View):
 
     template_name = 'sales/add_item.html'
     login_url = '/login'
